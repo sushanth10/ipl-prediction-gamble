0e2ab0740ae4443905b7503ffa5a8dce8780915f
Sushanth Bhat <sushanthbhat15@gmail.com>
2025-03-24 23:42:38 +0530

    changed styling and updated results

--- Dashboard.py
+++ Dashboard.py
@@ -70,6 +70,9 @@ def calculate_scores(results_df, predictions):
 
     return pd.DataFrame(leaderboard).sort_values(by="Points", ascending=False), points_progression
 
+def format_matchwise_points(points_list):
+    return "".join(f'<span style="background-color:#444; color:white; padding:5px 8px; border-radius:5px; margin:2px;">{p}</span>' for p in points_list)
+
 def matchwise_predictions(schedule_df, predictions):
     """Generate a matchwise prediction table."""
     matchwise_data = []
@@ -154,6 +157,24 @@ def main():
             .tick { color: limegreen; font-size: 18px; }
             .cross { color: red; font-size: 18px; }
             .dash { color: grey; font-size: 18px; }
+                
+            .matchwise-points {
+                display: flex;
+                gap: 6px;
+            }
+
+            .matchwise-points span {
+                padding: 4px 12px;
+                border-radius: 50px;  /* Makes it a pill */
+                font-weight: bold;
+                font-size: 14px;
+                color: white;
+                background-color: #17a2b8; /* Single color (Teal) */
+                min-width: 35px;
+                text-align: center;
+                display: inline-block;
+            }
+
         </style>
         """, unsafe_allow_html=True)
 
@@ -168,14 +189,18 @@ def main():
     results_df = load_results(results_path)
     predictions = load_predictions(predictions_path)
     leaderboard_df, points_progression = calculate_scores(results_df, predictions)
+    leaderboard_df["Rank"] = leaderboard_df["Points"].rank(method="dense", ascending=False).astype(int)
+    leaderboard_df = leaderboard_df.set_index("Rank")
+    leaderboard_df["Matchwise Points (Last 5)"] = leaderboard_df["Matchwise Points (Last 5)"].apply(format_matchwise_points)
     matchwise_df = matchwise_predictions(schedule_df, predictions)
 
     tab1, tab2, tab3 = st.tabs(["Leaderboard", "All Predictions", "Matchwise Predictions"])
+    # tab1, tab2, tab3, tab4 = st.tabs(["Leaderboard", "All Predictions", "Matchwise Predictions", "Analysis"])
 
     with tab1:         
         st.subheader("Leaderboard")
-        st.dataframe(leaderboard_df, use_container_width=True, hide_index=True)
-        st.markdown("##### Remaining Match Total Possible Outcomes for League Matches : **147 quintillion** (147,573,952,589,676,412,928) outcomes") 
+        st.write(leaderboard_df.to_html(escape=False, index=True), use_container_width=True, hide_index=True, unsafe_allow_html=True)
+        st.markdown("##### Total Possible Outcomes for Remaining League Matches : **73.7 quintillion** (73,786,976,294,838,206,464) outcomes") 
 
         st.write('\n\n')
         st.subheader("Points Progression (Worm Graph)")
